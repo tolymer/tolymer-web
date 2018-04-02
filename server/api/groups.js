f353529fbb0a72ecaace5469131cc2393a9d8e6b
1000ch <shogo.sensui@gmail.com>
2018-04-02 23:10:58 +0900

    Create groups API endpoint

---
+++ server/api/groups.js
@@ -0,0 +1,67 @@
+const axios = require('axios')
+const apiBaseUrl = process.env.API_BASE_URL || 'http://localhost:3000'
+
+module.exports = {
+  create: {
+    path: '/groups',
+    handler: async (req, res) => {
+      const { name, description } = req.body
+      const { headers } = req
+
+      try {
+        const { data } = await axios.post(`${apiBaseUrl}/groups`, {
+          name,
+          description
+        }, {
+          headers
+        })
+
+        res.status(201).json(data)
+      } catch (e) {
+        res.status(404).json({})
+      }
+    }
+  },
+  read: {
+    path: '/groups/:groupId',
+    handler: async (req, res) => {
+      const { groupId } = req.params
+      const { headers } = req
+
+      try {
+        const { data } = await axios.get(`${apiBaseUrl}/groups/${groupId}`, {
+          headers
+        })
+
+        res.status(200).json(data)
+      } catch (e) {
+        res.status(404).json({})
+      }
+    }
+  },
+  update: {
+    path: '/groups/:groupId',
+    handler: async (req, res) => {
+      const { groupId } = req.params
+      const { name, description } = req.body
+      const { headers } = req
+
+      try {
+        const { data } = await axios.post(`${apiBaseUrl}/groups/${groupId}`, {
+          name,
+          description
+        }, {
+          headers
+        })
+
+        res.status(201).json(data)
+      } catch (e) {
+        res.status(404).json({})
+      }
+    }
+  },
+  delete: {
+    path: '/groups/*',
+    handler: async (req, res) => res.status(403)
+  }
+}
