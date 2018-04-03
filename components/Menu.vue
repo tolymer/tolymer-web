1b0d520a83f9ab3f4c20a1e9d1f57da64197bb5a
1000ch <shogo.sensui@gmail.com>
2018-04-03 11:45:35 +0900

    Fix links

--- components/Menu.vue
+++ components/Menu.vue
@@ -1,6 +1,7 @@
 <template>
   <ul>
     <li><router-link to="/">Index</router-link></li>
-    <li><router-link to="/new">Sign up</router-link></li>
+    <li><router-link to="/users/new">Sign up</router-link></li>
+    <li><router-link to="/groups/new">Create group</router-link></li>
   </ul>
 </template>
