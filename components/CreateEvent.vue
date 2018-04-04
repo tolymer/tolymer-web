2de15b5161377c266841f5850f42bc580295dfb6
1000ch <shogo.sensui@gmail.com>
2018-04-04 23:24:41 +0900

    CreateEvent form

---
+++ components/CreateEvent.vue
@@ -0,0 +1,59 @@
+<template>
+  <form>
+    <label>
+      Title
+      <input
+        v-model="title"
+        type="text">
+    </label>
+    <label>
+      Description
+      <input
+        v-model="description"
+        type="text">
+    </label>
+    <label>
+      Date
+      <input
+        v-model="date"
+        type="date">
+    </label>
+    <button
+      type="submit"
+      @click="onClick">
+      Create event
+    </button>
+  </form>
+</template>
+
+<script>
+  import { parse } from 'cookie'
+
+  export default {
+    data() {
+      return {
+        title: '',
+        description: '',
+        date: ''
+      }
+    },
+    methods: {
+      async onClick(e) {
+        e.preventDefault()
+
+        const { accessToken } = parse(document.cookie)
+
+        await this.$store.dispatch('event/createEvent', {
+          title: this.title,
+          description: this.description,
+          date: this.date,
+          accessToken
+        })
+
+        this.title = ''
+        this.description = ''
+        this.date = ''
+      }
+    }
+  }
+</script>
--- pages/events/new.vue
+++ pages/events/new.vue
@@ -2,66 +2,18 @@
   <section>
     <h1>New Event</h1>
     <Menu />
-    <form>
-      <label>
-        Title
-        <input
-          v-model="title"
-          type="text">
-      </label>
-      <label>
-        Description
-        <input
-          v-model="description"
-          type="text">
-      </label>
-      <label>
-        Date
-        <input
-          v-model="date"
-          type="date">
-      </label>
-      <button
-        type="submit"
-        @click="onClick">
-        Create event
-      </button>
-    </form>
+    <CreateEvent />
   </section>
 </template>
 
 <script>
-  import { parse } from 'cookie'
   import Menu from '~/components/Menu'
+  import CreateEvent from '~/components/CreateEvent'
 
   export default {
     components: {
-      Menu
-    },
-    data() {
-      return {
-        title: '',
-        description: '',
-        date: ''
-      }
-    },
-    methods: {
-      async onClick(e) {
-        e.preventDefault()
-
-        const { accessToken } = parse(document.cookie)
-
-        await this.$store.dispatch('event/createEvent', {
-          title: this.title,
-          description: this.description,
-          date: this.date,
-          accessToken
-        })
-
-        this.title = ''
-        this.description = ''
-        this.date = ''
-      }
+      Menu,
+      CreateEvent
     }
   }
 </script>
