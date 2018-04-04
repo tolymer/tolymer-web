a1cd4991df19472f187437261aa99cf89e813e5a
1000ch <shogo.sensui@gmail.com>
2018-04-04 23:19:35 +0900

    CreateUser form

--- pages/users/new.vue
+++ pages/users/new.vue
@@ -2,53 +2,18 @@
   <section>
     <h1>New User</h1>
     <Menu />
-    <form>
-      <label>
-        Name
-        <input
-          v-model="name"
-          type="text">
-      </label>
-      <label>
-        Password
-        <input
-          v-model="password"
-          type="password">
-      </label>
-      <button
-        type="submit"
-        @click="onClick">
-        Create user
-      </button>
-    </form>
+    <CreateUser />
   </section>
 </template>
 
 <script>
   import Menu from '~/components/Menu'
+  import CreateUser from '~/components/CreateUser'
 
   export default {
     components: {
-      Menu
-    },
-    data() {
-      return {
-        name: '',
-        password: ''
-      }
-    },
-    methods: {
-      async onClick(e) {
-        e.preventDefault()
-
-        await this.$store.dispatch('user/createUser', {
-          name: this.name,
-          password: this.password
-        })
-
-        this.name = ''
-        this.password = ''
-      }
+      Menu,
+      CreateUser
     }
   }
 </script>
