71fefd3cb9f89ab9c0a17409b2a6da1582cd3a2e
Kazuhito Hokamura <k.hokamura@gmail.com>
2018-04-11 21:26:31 +0900

    npm run lint:css -- --fix

--- components/Header.vue
+++ components/Header.vue
@@ -13,8 +13,8 @@
 <style>
 header {
   padding: var(--space-base) calc(var(--space-base) * 2);
-  background-color: var(--color-black);
   color: var(--color-white);
+  background-color: var(--color-black);
 }
 
 .Title {
