780ebb1cbc5ab97efbde12e53ad54b1cd7e607ad
1000ch <shogo.sensui@gmail.com>
2018-04-29 20:19:18 +0900

    Do not request current_user if data exists

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -2,11 +2,12 @@ export default async context => {
   const { accessToken } = context.cookie;
 
   if (accessToken) {
-    await context.store.dispatch('loggedIn');
-
-    await context.store.dispatch('getCurrentUser', {
-      accessToken
-    });
+    if (!context.store.state.id) {
+      await context.store.dispatch('loggedIn');
+      await context.store.dispatch('getCurrentUser', {
+        accessToken
+      });
+    }
   } else {
     await context.store.dispatch('logout');
 
